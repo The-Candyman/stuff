63c97be5e051548f701fa841a86f87a2956eedd4
The-Candyman <147785024+The-Candyman@users.noreply.github.com>
2023-11-17 16:48:22 -0500

    Create animator.js
    handles actually animating things

---
+++ animator/animator.js
@@ -0,0 +1,63 @@
+const inputs = document.querySelector("#inputs"); // not used yet
+const wrapper = document.querySelector(".wrapper");
+const display = document.querySelector("#display");
+const preview = document.querySelector("#preview");
+const inputRenderer = inputs.getContext("2d");
+const pbTop = preview.getContext("2d");
+const pbBottom = display.getContext("2d");
+
+let objects = [];
+
+function clearAll() {
+  objects = [];
+}
+
+function render() {
+  //let benchmark = Date.now();
+  pbTop.clearRect(0, 0, preview.width, preview.height);
+  pbBottom.clearRect(0, 0, display.width, display.height);
+  for (let i = 0; i < objects.length; i++) {
+    objects[i].draw(false, pbBottom);
+  }
+  //console.log(Date.now() - benchmark);
+}
+
+function previewRender(object) {
+  //let benchmark = Date.now();
+  pbTop.clearRect(0, 0, preview.width, preview.height);
+  object.draw(true, pbTop);
+  //console.log(Date.now() - benchmark);
+}
+
+let lastPos = [];
+let lastTime;
+
+let drawing = false;
+let drawingMode = {smooth: true, closed: true};
+
+["mousemove", "touchmove"].forEach(qwerty => window.addEventListener(qwerty, (e) => {
+  let x = e.clientX - wrapper.offsetLeft;
+  let y = e.clientY - wrapper.offsetTop;
+  if (x < 0  ||  x > preview.width  ||  y < 0  ||  y > preview.height) {
+    drawing = false;
+    lastPos = [];
+  } else if ((drawing  &&  (Date.now() - lastTime > 20  ||  lastPos.length === 0  ||  Math.abs(x - lastPos[0]) > 2  ||  Math.abs(y - lastPos[1]) > 2))) {
+    lastPos = [x, y];
+    lastTime = Date.now();
+    objects[objects.length - 1].addPoint([x,y]);
+    // objects[objects.length - 1].draw();
+  }
+}));
+
+["mousedown", "touchdown"].forEach(qwerty => wrapper.addEventListener(qwerty, (e) => {
+  drawing = true;
+  lastPos = [e.offsetX, e.offsetY];
+  lastTime = Date.now();
+  console.log(objects[objects.push(new LineShape([[e.offsetX, e.offsetY]], drawingMode)) - 1]);
+}));
+
+["mouseup", "touchup"].forEach(qwerty => window.addEventListener(qwerty, () => {
+  drawing = false;
+  lastPos = [];
+  render();
+}));
